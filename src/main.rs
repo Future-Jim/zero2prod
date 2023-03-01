a9483baff5604a93c279beb5abc85569e4555390
James Arias <jamesarias@protonmail.com>
2023-03-01 18:42:29 -0500

    adding rustfmt.toml and updating main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -7,16 +7,15 @@ async fn greet(req: HttpRequest) -> impl Responder {
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
-
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-	App::new()
-	    .route("/", web::get().to(greet))
-	    .route("/{name}", web::get().to(greet))
-	    .route("/health_check", web::get().to(health_check))
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
     })
     .bind("127.0.0.1:8000")?
     .run()
