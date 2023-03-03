9c2aea5930db521c4158ed06043371c85da65a08
James Arias <jamesarias@protonmail.com>
2023-03-03 09:05:49 -0500

    rustfmt on lib and main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 //! main.rs
 
-use zero2prod::run;
 use std::net::TcpListener;
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind to random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
     println!("The server is listening on 127.0.0.1:{}", port);
     run(listener)?.await
-	
 }
-
-
