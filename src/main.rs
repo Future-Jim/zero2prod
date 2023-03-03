3fb82e5d7297af0dbc08dc39b82c20b08d439f4e
James Arias <jamesarias@protonmail.com>
2023-03-02 19:27:29 -0500

    updating main, lib and tests to use random port with TcpListener

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 //! main.rs
 
 use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("The server is listening on 127.0.0.1:{}", port);
+    run(listener)?.await
+	
 }
