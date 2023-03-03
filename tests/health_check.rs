c0869e1e1619b6258605eb8576bb4709d8f89331
James Arias <jamesarias@protonmail.com>
2023-03-03 09:14:10 -0500

    fixing clippy identified error

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -20,6 +20,6 @@ fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let server = zero2prod::run(listener).expect("Failed to bind to address");
-    let _ = tokio::spawn(server);
+    tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
