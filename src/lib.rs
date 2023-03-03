a35b087afe6ffe7d9c3687e0da7646639a0d3705
James Arias <jamesarias@protonmail.com>
2023-03-03 18:32:07 -0500

    fixing formatting on lib and health_check

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@ use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
 #[derive(serde::Deserialize)]
-struct FormData{
+struct FormData {
     email: String,
-    name: String
+    name: String,
 }
 
 async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
@@ -18,12 +18,12 @@ async fn health_check() -> HttpResponse {
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
-	App::new()
-	    .route("/health_check", web::get().to(health_check))
-	// A new entry in our routing table for POST /subscriptions requests
-	    .route("/subscriptions", web::post().to(subscribe))
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
     })
-	.listen(listener)?
-	.run();
+    .listen(listener)?
+    .run();
     Ok(server)
 }
