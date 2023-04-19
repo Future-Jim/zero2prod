e84bdf97f30bbfab32e5e02d0b2d6345df8012c0
future-jim <jamesarias@protonmail.com>
2023-04-19 14:30:39 -0400

    Update health_check.rs1

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,5 +1,8 @@
 use actix_web::{HttpRequest, HttpResponse, Responder};
 
 pub async fn health_check(_req: HttpRequest) -> impl Responder {
+//this is to create a merge conflict
     println!("helo");
+    println!("hello");
+//this is to create a merge conflict
 }
