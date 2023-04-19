57b5540fdd470aedfa1af8c9c3552b4fd2ca5d47
James Arias <jamesarias@protonmail.com>
2023-04-19 14:31:48 -0400

    updating routes

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -2,7 +2,7 @@ use actix_web::{HttpRequest, HttpResponse, Responder};
 
 pub async fn health_check(_req: HttpRequest) -> impl Responder {
 //this is to create a merge conflict
-    println!("helo");
+    println!("heldfsdfdsfdso");
     println!("hello");
 //this is to create a merge conflict
 }
