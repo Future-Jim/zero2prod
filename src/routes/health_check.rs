be24473e46c6c3e14d09457450ef05fbe6787588
James Arias <jamesarias@protonmail.com>
2023-04-19 14:43:28 -0400

    creating a merge conflict

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,5 +1,7 @@
 use actix_web::{HttpRequest, HttpResponse, Responder};
 
 pub async fn health_check(_req: HttpRequest) -> impl Responder {
-    HsttpResponse::Ok().finish();
+    //this is a conflict
+    HttpResponse::Ok().finish();
+    //this will create a conflict
 }
