56504f2ece2e1969b563dc0dfafaeaf7eaf7576c
James Arias <jamesarias@protonmail.com>
2023-04-24 10:44:54 -0400

    rust fmt

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,3 @@
-
 use chrono::Utc;
 use uuid::Uuid;
 
@@ -11,22 +10,21 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(
-    _form: web::Form<FormData>,
-    _pool: web::Data<PgPool>,
-) -> HttpResponse {
+pub async fn subscribe(_form: web::Form<FormData>, _pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
     log::info!(
-	"request_id {} - Adding '{}' '{}' as a new subscriber.",
-	request_id,
-	_form.email,
-	_form.name,
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        _form.email,
+        _form.name,
     );
-    log::info!("request id {} - Saving new subscriber details in the database",
-	       request_id);
-    
+    log::info!(
+        "request id {} - Saving new subscriber details in the database",
+        request_id
+    );
+
     match sqlx::query!(
-	r#"
+        r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
         "#,
@@ -38,15 +36,20 @@ pub async fn subscribe(
     .execute(_pool.get_ref())
     .await
     {
-	Ok(_) => {
-	    log::info!("request id {} - New subscriber details have been saved",
-	    request_id);
-	    HttpResponse::Ok().finish()},
-	Err(e) => {
-	    log::error!("request id {} - Failed to execute query: {:?}",
-			request_id,
-			e);
-	    HttpResponse::InternalServerError().finish()
-	}
+        Ok(_) => {
+            log::info!(
+                "request id {} - New subscriber details have been saved",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        }
+        Err(e) => {
+            log::error!(
+                "request id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
+            HttpResponse::InternalServerError().finish()
+        }
     }
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,8 +1,8 @@
-use sqlx::{PgConnection, PgPool, Connection, Executor};
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
+use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
-use uuid::Uuid;
 
 pub struct TestApp {
     pub address: String,
@@ -32,35 +32,33 @@ async fn spawn_app() -> TestApp {
 
     let mut configuration = get_configuration().expect("Failed to read configuration");
     configuration.database.database_name = Uuid::new_v4().to_string();
-    let connection_pool = configure_database(&configuration.database)
-	.await;
-     
-    let server = run(listener, connection_pool.clone())
-	.expect("Failed to bind to address");
-    
+    let connection_pool = configure_database(&configuration.database).await;
+
+    let server = run(listener, connection_pool.clone()).expect("Failed to bind to address");
+
     let _ = tokio::spawn(server);
     TestApp {
-	address,
-	db_pool: connection_pool,
+        address,
+        db_pool: connection_pool,
     }
 }
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     let mut connection = PgConnection::connect(&config.connection_string_without_db())
         .await
-	.expect("Failed to connect to Postgres");
+        .expect("Failed to connect to Postgres");
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
-	.expect("Failed to create database");
+        .expect("Failed to create database");
 
     let connection_pool = PgPool::connect(&config.connection_string())
         .await
-	.expect("Failed to connect to Postgres.");
+        .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./scripts/migrations")
         .run(&connection_pool)
         .await
-	.expect("Failed to migrate the database");
+        .expect("Failed to migrate the database");
     connection_pool
 }
 
@@ -68,7 +66,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app = spawn_app().await;
-   
+
     let client = reqwest::Client::new();
     // Act
     let body = "name=jmes%20guin&email=james%40gmail.com";
@@ -82,12 +80,12 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     assert_eq!(200, response.status().as_u16());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
-	.fetch_one(&app.db_pool)
-	.await
-	.expect("Failed to fetch saved subscription.");
-    
-   assert_eq!(saved.email, "james@gmail.com");
-   assert_eq!(saved.name, "jmes guin");
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "james@gmail.com");
+    assert_eq!(saved.name, "jmes guin");
 }
 
 #[tokio::test]
