1cf660e3a5ab54d6219bfaf0e493ed8e53c24f52
James Arias <jamesarias@protonmail.com>
2023-04-25 18:17:01 -0400

    on page 91 of book

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,6 @@
 use chrono::Utc;
 use uuid::Uuid;
-
+use tracing::Instrument;
 use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
 
@@ -12,17 +12,23 @@ pub struct FormData {
 
 pub async fn subscribe(_form: web::Form<FormData>, _pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        _form.email,
-        _form.name,
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        _subscriber_email = %_form.email,
+        _subscriber_name= %_form.name,
     );
-    log::info!(
+    tracing::info!(
         "request id {} - Saving new subscriber details in the database",
         request_id
     );
 
+    let request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!(
+	"Saving new subscriber details in the database"
+    );
+    
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -33,18 +39,19 @@ pub async fn subscribe(_form: web::Form<FormData>, _pool: web::Data<PgPool>) -> 
         _form.name,
         Utc::now()
     )
-    .execute(_pool.get_ref())
-    .await
+	.execute(_pool.get_ref())
+	.instrument(query_span)
+	.await
     {
         Ok(_) => {
-            log::info!(
+            tracing::info!(
                 "request id {} - New subscriber details have been saved",
                 request_id
             );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!(
+            tracing::error!(
                 "request id {} - Failed to execute query: {:?}",
                 request_id,
                 e
